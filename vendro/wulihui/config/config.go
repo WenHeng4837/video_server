990cd07d464526e8378b2d30bc849f0960e74f9f
WenHneg <1967014983@qq.com>
2021-07-19 16:15:37 +0800

    first2

--- vendro/wulihui/config/config.go
+++ vendro/wulihui/config/config.go
@@ -1,33 +1,28 @@
 package config
 
-import (
-	"encoding/json"
-	"os"
-)
-
 //配置
 //阿里云上配置负载均衡
-type Configuration struct {
-	LBAddr  string `json:"lb_addr"`
-	OssAddr string `json:"oss_addr"`
-}
-
-var configuration *Configuration
-
-func init() {
-	file, _ := os.Open("./config.json")
-	defer file.Close()
-	decoder := json.NewDecoder(file)
-	configuration = &Configuration{}
-	err := decoder.Decode(configuration)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func GetLbAddr() string {
-	return configuration.LBAddr
-}
-func GetOssAddr() string {
-	return configuration.OssAddr
-}
+//type Configuration struct {
+//	LBAddr  string `json:"lb_addr"`
+//	OssAddr string `json:"oss_addr"`
+//}
+//
+//var configuration *Configuration
+//
+//func init() {
+//	file, _ := os.Open("./config.json")
+//	defer file.Close()
+//	decoder := json.NewDecoder(file)
+//	configuration = &Configuration{}
+//	err := decoder.Decode(configuration)
+//	if err != nil {
+//		panic(err)
+//	}
+//}
+//
+//func GetLbAddr() string {
+//	return configuration.LBAddr
+//}
+//func GetOssAddr() string {
+//	return configuration.OssAddr
+//}
